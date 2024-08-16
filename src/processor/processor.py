b84c8a16b603d33dac3b49003240bc243a57ad80
Mohamed Shahawy <mohamedshahawy@aucegypt.edu>
2024-08-16 16:35:06 +0100

    Fixed refactored module hierarchy

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -8,11 +8,11 @@ from .parser import parse_code_cell, parse_markdown_cell
 
 def process_notebook(notebook_path, output_path, root_package=''):
 
-    # read nb
+    # read notebook
     unparsed_cells = read_notebook(notebook_path)
     
     # build module tree
-    root = ModuleNode(root_package)
+    root = ModuleNode(root_package, 3)
     current_node = root
     node_stack = [root]
     
@@ -24,19 +24,21 @@ def process_notebook(notebook_path, output_path, root_package=''):
         if cell.cell_type == 'markdown':
             parsed_md = parse_markdown_cell(cell.cell_idx, cell.raw_source)
             for header in parsed_md.headers:
-                header_name = header.name.replace(' ', '_').lower()
+                header_name = header.name.replace(' ', '_').replace('-', '_').lower()
 
                 while len(node_stack) > header.level:
                     node_stack.pop()
 
-                if header_name not in node_stack[-1].children:
-                    new_node = ModuleNode(header_name, node_stack[-1])
-                    node_stack[-1].add_child(new_node)
-                    node_stack.append(new_node)
-                else:
-                    node_stack.append(node_stack[-1].children[header_name])
+                # if we're on the same level as the stack's current node,
+                # we move back to the correct parent level
+                if len(node_stack) == header.level:
+                    node_stack.pop()
 
-                current_node = node_stack[-1]
+                new_node = ModuleNode(header_name, node_stack[-1])
+                node_stack[-1].add_child(new_node)
+                node_stack.append(new_node)
+
+                current_node = new_node
 
             current_node.add_parsed_cell(parsed_md)
 
@@ -44,5 +46,7 @@ def process_notebook(notebook_path, output_path, root_package=''):
             parsed_code = parse_code_cell(cell.cell_idx, cell.raw_source, current_node, global_definitions)
             current_node.add_parsed_cell(parsed_code)
 
-    # write
+    # write the parsed module tree
     write_modules(root, output_path)
+
+    return root
