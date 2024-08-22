6c5af9c212dbfc0deaa155f9be6d84eb11eafdce
Mohamed Shahawy <mohamedshahawy@aucegypt.edu>
2024-08-22 16:28:01 +0100

    Modified Markdown parsing to maintain order of execution

--- processor/parser.py
+++ processor/parser.py
@@ -47,40 +47,50 @@ def parse_markdown_cell(cell_idx, source):
 
     md_str = copy(source)   # copied to prevent in-place modification
 
-    # html multiline comment pattern
     comment_regex = re.compile(r'<!--(?P<comment>(?:(.|\n)*?))-->')
-
-    comments = []
-    for match in comment_regex.finditer(md_str):
-        comments.append(match.group('comment'))
-
-        # removing the html comments from the MD
-        md_str = md_str.replace(match.group(0), '')
-
-    # handle markdown / module hierarchy (md_str is stripped of html comments)
-    md_headers = []
-    for line in md_str.split('\n'):
-        if line.startswith('#'):
-            md_level = line.count('#')
-            md_header = line.strip('#').strip()
-
-            md_headers.append(MarkdownHeader(md_header, md_level))
-    
-    # cmds pattern
     cmd_regex = re.compile(r'^\$(?P<command>\w+(?:-\w+)*)(?:=(?P<value>.*))?$', re.MULTILINE)
 
-    commands = []
-    for comment in comments:
-        for line in comment.split('\n'):
-            # strip leading/trailing whitespaces for the cmd=val pattern
-            for cmd_match in cmd_regex.finditer(line.strip()):
-                cmd_str = cmd_match.group('command')
-                value = cmd_match.group('value')
-                
-                try:
-                    commands.append(Command(cmd_str, value))
-                except ValueError:
-                    # invalid command, warn and ignore
-                    print(f'An invalid command type `{cmd_str}` in Cell {cell_idx} was found and ignored. (Headers encountered: "{md_header}")')
+    md_elements = []        # both the Command + MarkdownHeader objects, in the order they appear in
+                            # we don't simlpy match and extract commands/headers across the raw source
+                            # to maintain the order of execution (in case multiple 
+                            # headers/commands are present)
 
-    return ParsedMarkdownCell(md_headers, commands)
+
+    # line by line parsing (to maintain sequential order)
+    for line in md_str.split('\n'):
+        clean_line = line.strip()
+
+        # HTML comments' matching (potentially Command objects)
+        comment_match = comment_regex.search(clean_line)
+        if comment_match:
+            comment = comment_match.group('comment')
+            comment_lines = comment.split('\n')
+
+            for comment_line in comment_lines:
+                # parse comment lines (we're allowing multiple commands 
+                # in a single comment block)
+
+                cmd_match = cmd_regex.match(comment_line.strip())
+                if cmd_match:
+                    cmd_str = cmd_match.group('command')
+                    value = cmd_match.group('value')
+                    
+                    try:
+                        md_elements.append(Command(cmd_str, value))
+                    except ValueError:
+                        print((
+                            f'WARNING: An invalid command type `{cmd_str}` in'
+                            f' Cell #{cell_idx} was found and ignored.'
+                        ))
+
+            continue
+
+        # MD header -> extract node depth/level and name of package/module
+        if clean_line.startswith('#'):
+            md_level = clean_line.count('#')
+            md_header = clean_line.strip('#').strip()
+            md_elements.append(MarkdownHeader(md_header, md_level))
+
+    return ParsedMarkdownCell(md_elements)
+
+
